db16b2d87d7557a1443a63d2bf4c096afb23481f
LnlyPineapple27 <47707507+LnlyPineapple27@users.noreply.github.com>
2021-12-27 21:59:09 +0700

    add Inversion of Controls method

---
+++ IoCContainer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPFramework
+{
+    class IoCContainer
+    {
+        private static readonly Dictionary<Type, object> registered_dependency = new Dictionary<Type, object>();
+
+        public static void SetDependency<TInterface, TModule>()
+        {
+            SetDependency(typeof(TInterface), typeof(TModule));
+        }
+        private static void SetDependency(Type interfaceType, Type dependencyType)
+        {
+            if (!interfaceType.IsAssignableFrom(dependencyType))
+            {
+                throw new Exception("Dependency không tương thích với dạng Interface được truyền!");
+            }
+
+            // Lấy constructor của dependency
+            var firstConstructor = dependencyType.GetConstructors()[0];
+            object module = null;
+            //Nếu dạng constructor cần parameter 
+            if (firstConstructor.GetParameters().Any())
+            {
+                var constructorParameters = firstConstructor.GetParameters(); //db, log, es
+                var moduleDependecies = new List<object>();
+                foreach (var parameter in constructorParameters)
+                {
+                    var dependency = GetDependency(parameter.ParameterType);
+                    moduleDependecies.Add(dependency);
+                }
+                module = firstConstructor.Invoke(moduleDependecies.ToArray());
+            }
+            else
+            {
+                module = firstConstructor.Invoke(null);
+            }
+            registered_dependency.Add(interfaceType, module);
+        }
+        public static T GetDependency<T>()
+        {
+            return (T)GetDependency(typeof(T));
+        }
+
+        private static object GetDependency(Type interfaceType)
+        {
+            if (registered_dependency.ContainsKey(interfaceType))
+            {
+                return registered_dependency[interfaceType];
+            }
+            throw new Exception("Dependency chưa được đăng kí");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,45 +23,50 @@ namespace SEPFramework
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            List<SEPButton> buttons = new();
+            //List<SEPButton> buttons = new();
 
-            FactoryButton factory = new FactoryButtonInsert();
-            buttons.Add(factory.CreateButton("btnInsert"));
+            //FactoryButton factory = new FactoryButtonInsert();
+            //buttons.Add(factory.CreateButton("btnInsert"));
 
-            factory = new FactoryButtonDelete();
-            buttons.Add(factory.CreateButton("btnUpdate"));
+            //factory = new FactoryButtonDelete();
+            //buttons.Add(factory.CreateButton("btnUpdate"));
 
-            List<Dictionary<string, string>> data = new()
-            {
-                new()
-                {
-                    ["col1"] = "cell1",
-                    ["col2"] = "cell2",
-                    ["col3"] = "cell3",
-                    ["col4"] = "cell4",
-                    ["col5"] = "cell5",
-                },
-                new()
-                {
-                    ["col1"] = "cell6",
-                    ["col2"] = "cell7",
-                    ["col3"] = "cell8",
-                    ["col4"] = "cell9",
-                    ["col5"] = "cell0",
-                }
-            };
+            //List<Dictionary<string, string>> data = new()
+            //{
+            //    new()
+            //    {
+            //        ["col1"] = "cell1",
+            //        ["col2"] = "cell2",
+            //        ["col3"] = "cell3",
+            //        ["col4"] = "cell4",
+            //        ["col5"] = "cell5",
+            //    },
+            //    new()
+            //    {
+            //        ["col1"] = "cell6",
+            //        ["col2"] = "cell7",
+            //        ["col3"] = "cell8",
+            //        ["col4"] = "cell9",
+            //        ["col5"] = "cell0",
+            //    }
+            //};
 
-            SEPDataGridView dataGridView = new(data);
+            //SEPDataGridView dataGridView = new(data);
 
-            FactoryPanel factoryPanel = new FactoryPanel();
-             
-            Panel panelGridView = factoryPanel.CreatePanelDataGridView("panelGridView", data);
-            Panel panelButtons = factoryPanel.CreateFLPanelDockRightButtons("panelButtons", buttons);
+            //FactoryPanel factoryPanel = new FactoryPanel();
+
+            //Panel panelGridView = factoryPanel.CreatePanelDataGridView("panelGridView", data);
+            //Panel panelButtons = factoryPanel.CreateFLPanelDockRightButtons("panelButtons", buttons);
 
 
 
-            Application.Run(new FactoryFormVertical().CreateLoginForm("loginForm"));
+            //Application.Run(new FactoryFormVertical().CreateLoginForm("loginForm"));
             //Application.Run(new SEPForm("DataView", "DataView", SEPForm.Type.Main, "DataView", SEPForm.SEPFormDefaultSize, panelButtons, panelGridView));
+            IoCContainer.SetDependency<FactoryForm, FactoryFormVertical>();
+
+
+            Application.Run(IoCContainer.GetDependency<FactoryForm>().CreateLoginForm("Login form"));
+
         }
     }
 }
