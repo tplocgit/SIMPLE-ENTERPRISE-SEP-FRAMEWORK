e6b399db0f427b98c3e8a2052b04da57b225522d
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2022-01-09 19:14:01 +0700

    RecordView Form

--- Forms/RecordViewForm.cs
+++ Forms/RecordViewForm.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using SEPFramework.Buttons;
+using SEPFramework.Factories;
+using SEPFramework.FormControls;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SEPFramework.Buttons;
-using SEPFramework.Factories;
+using System.Windows.Forms;
 
 namespace SEPFramework.Forms
 {
     class RecordViewForm : SEPForm
     {
-        enum SaveType
+        public enum SaveType
         {
             Insert = 0,
             Update = 1,
@@ -28,16 +27,30 @@ namespace SEPFramework.Forms
 
             });
 
-            SEPButton btnCancel= new("btnDelete", "Delete", (sender, agrs) =>
-            {
-                Debug.WriteLine("Cancel");
-            });
+            SEPButton btnCancel = new("btnDelete", "Delete", (sender, agrs) =>
+             {
+                 Debug.WriteLine("Cancel");
+             });
 
 
             FactoryPanel factoryPanel = new();
 
-            this._panelButtons = factoryPanel.CreateFLPanelDockRightButtons("btnPanel", new List<SEPButton> { btnInsert, btnDelete, btnReload });
+            this._panelButtons = factoryPanel.CreateFLPanelDockRightButtons("btnPanel", new List<SEPButton> { btnInsert, btnCancel });
         }
 
+        public RecordViewForm(string name, string tableLabel, string tableTitle, SaveType type, DataGridViewRow row) : this(name, tableLabel, tableTitle, type)
+        {
+            DataGridViewRow headers = row.DataGridView.Rows[0];
+            Dictionary<string, string> fields = Enumerable
+                .Range(0, headers.Cells.Count)
+                .ToDictionary(i => headers.Cells[i].ToString(), i => type == SaveType.Update ? row.Cells[i].ToString() : "");
+
+            FactoryPanel factoryPanel = new();
+
+            this._panelMain = factoryPanel
+                .CreateTLPabelDockFillFormControls("rowPanel", FormControl.CreateFormControlList(fields));
+
+            this.SetUpForm();
+        }
     }
 }
